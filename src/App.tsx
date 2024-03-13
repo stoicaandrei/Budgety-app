90f1829d0badb5173587d72fd49927dbe91f14cb
Andrei Stoica <andrei@stoica.dev>
2024-03-13 18:22:04 +0200

    Create BaseLayout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <div>Hello Budgety</div>;
-}
+import React from "react";
+import { Button, Card } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
+import BaseLayout from "./layouts/BaseLayout";
+
+const App = () => (
+  <BaseLayout>
+    <Card>
+      <Button type="primary">Button</Button>
+      <SmileOutlined />
+    </Card>
+  </BaseLayout>
+);
 
 export default App;
