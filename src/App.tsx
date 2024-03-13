40ea33081b3049028c77a4e0d3f7824c1f9f7fad
Andrei Stoica <andrei@stoica.dev>
2024-03-13 18:37:26 +0200

    Add routing to project

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import Dashboard from "./pages/Dashboard";
 import Budget from "./pages/Budget";
 import Transactions from "./pages/Transactions";
 
+import { Routes, Route, Navigate } from "react-router-dom";
+
 const App = () => {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/budget" element={<Budget />} />
+      <Route path="/transactions" element={<Transactions />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default App;
